8d3d092807e7a518577d5a682be4d13a654d6f3d
rprojetos.ti@hotmail.com <rprojetos.ti@hotmail.com>
2023-12-12 22:29:22 -0300

    initial commit

---
+++ src/main/kotlin/br/com/rprojetos/Greeting.kt
@@ -0,0 +1,3 @@
+package br.com.rprojetos
+
+class Greeting(val id: Long, val content: String)
---
+++ src/main/kotlin/br/com/rprojetos/Startup.kt
@@ -0,0 +1,12 @@
+package br.com.rprojetos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Startup
+
+fun main(args: Array<String>) {
+	runApplication<Startup>(*args)
+}
+
